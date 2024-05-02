6d3486f629b5f8089f45325c1ab1f16de696145c
szbalint00 <szbalint2006@gmail.com>
2024-05-02 11:43:08 +0200

    iuh

---
+++ MongoDB/ejemplo01.sql
@@ -0,0 +1,29 @@
+//types to organize documents
+
+{
+    field:"value",
+    field2:"value2"
+}
+
+// Datas about a person
+
+{
+    name:"Balint",
+    age:"17",
+    status:"single",
+    hobbies: ["Drumming","Programming","Motorcycling"]
+}
+
+// subdocuments
+
+{
+    _id:"uiygodiug",
+    city:"Granada",
+    zip:"18005",
+    loc:{
+        y:33.45536,
+        x:45.4598
+    },
+    pop:10000,
+    state:"Andalusia"
+}
